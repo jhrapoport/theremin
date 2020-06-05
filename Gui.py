2cf9a76b447ba0ae5cfdbfff30ed4aed2e92654a
hackerrank <me@hackerrank.com>
2020-06-05 16:45:37 -0700

    added volume slider

--- Gui.py
+++ Gui.py
@@ -1,11 +1,11 @@
 import tkinter as tk
+import tkinter.font as font
 import const
 import Theremin
 
 
 class Gui:
     def __init__(self):
-        self.note_playing = None
         self.theremin = Theremin.Theremin(const.CANVAS_LENGTH / 2, const.CANVAS_HEIGHT)
         self.window = tk.Tk()
         self.config()
@@ -24,12 +24,28 @@ class Gui:
         self.window.destroy()
 
     def config(self):
+        self.config_window()
+        self.add_canvas()
+        self.add_widgets()
+
+    def config_window(self):
         self.window.configure(background=const.BG_COLOR)
         self.window.title("Theremin")
         self.window.protocol("WM_DELETE_WINDOW", self.on_close)
+
+    def add_canvas(self):
         canvas = tk.Canvas(self.window, width=const.CANVAS_LENGTH, height=const.CANVAS_HEIGHT,
                            background=const.CANVAS_COLOR)
-        canvas.grid(column=0, row=0)
+        canvas.grid(column=0, row=0, columnspan=5, rowspan=5)
         canvas.configure(cursor="hand1 black")
         canvas.bind("<Motion>", self.on_motion)
 
+    def add_widgets(self):
+        volume_slider = tk.Scale(self.window, command=lambda x: self.change_volume(x))
+        volume_slider.config(background="darkgray", from_=0.0, to=1.0, resolution=0.01, length=64)
+        volume_slider.config(label="Volume", orient=tk.HORIZONTAL, showvalue=0, sliderlength=15)
+        volume_slider.grid(column=6, row=3)
+        volume_slider.set(const.DEFAULT_VOLUME)
+
+    def change_volume(self, new_volume):
+        self.theremin.adjust_volume(float(new_volume))
--- Theremin.py
+++ Theremin.py
@@ -7,6 +7,7 @@ import const
 
 class Theremin:
     def __init__(self, max_freq_i, max_amp_i):
+        self.volume = const.DEFAULT_VOLUME
         self.amp = 0
         self.freq = 0
         self.max_freq_i = max_freq_i
@@ -17,7 +18,8 @@ class Theremin:
 
     # change the current sound playing
     def switch_sound(self, freq_i, amp_i):
-        self.amp = const.MAX_AMP * amp_i / self.max_amp_i
+        self.amp = self.volume * \
+                   const.MAX_AMP * amp_i / self.max_amp_i
         self.freq = const.MIN_FREQ + \
                     (const.MAX_FREQ - const.MIN_FREQ) * freq_i / self.max_freq_i
 
@@ -38,6 +40,9 @@ class Theremin:
         t = numpy.arange(frames) / self.samplerate
         t = t.reshape(-1, 1)
         outdata[:] = self.amp * numpy.sin(2 * numpy.pi * self.freq * t)
+
+    def adjust_volume(self, new_volume):
+        self.volume = new_volume
 
     def destruct(self):
         self.playing = False
