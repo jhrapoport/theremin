8318d9e8a8aaa8ea08136a1bf23485d287192581
hackerrank <me@hackerrank.com>
2020-06-05 14:45:44 -0700

    made canvas widget so whole window isn't instrument

--- Gui.py
+++ Gui.py
@@ -6,7 +6,7 @@ import Theremin
 class Gui:
     def __init__(self):
         self.note_playing = None
-        self.theremin = Theremin.Theremin(gui_const.WINDOW_LENGTH, gui_const.WINDOW_HEIGHT)
+        self.theremin = Theremin.Theremin(gui_const.CANVAS_LENGTH/2, gui_const.CANVAS_HEIGHT)
         self.window = tk.Tk()
         self.config()
         self.run()
@@ -15,16 +15,21 @@ class Gui:
         self.window.mainloop()
 
     def on_motion(self, event):
-        self.theremin.switch_sound(event.x, gui_const.WINDOW_HEIGHT - event.y - 1)
+        distance = abs(gui_const.CANVAS_LENGTH/2 - event.x)
+        height = gui_const.CANVAS_HEIGHT - event.y - 1
+        self.theremin.switch_sound(distance, height)
 
     def on_close(self):
         self.theremin.destruct()
         self.window.destroy()
 
     def config(self):
-        self.window.configure(cursor="hand1 black")
-        self.window.bind("<Motion>", self.on_motion)
-        self.window.geometry(gui_const.WINDOW_DIMENSIONS)
-        self.window.resizable(0, 0)
+        self.window.configure(background=gui_const.BG_COLOR)
         self.window.title("Theremin")
         self.window.protocol("WM_DELETE_WINDOW", self.on_close)
+        canvas = tk.Canvas(self.window, width=gui_const.CANVAS_LENGTH, height=gui_const.CANVAS_HEIGHT,
+                           background=gui_const.CANVAS_COLOR)
+        canvas.grid(column=0, row=0)
+        canvas.configure(cursor="hand1 black")
+        canvas.bind("<Motion>", self.on_motion)
+
